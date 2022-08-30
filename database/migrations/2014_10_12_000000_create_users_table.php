e7bd633aed8d6b7ae542e78d867116236561a144
Fazal Hakim <hakimfazal426@gmail.com>
2022-08-30 15:06:40 +0500

    constraint

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('department_id')->nullable()->constrained();; 
             $table->string('name');
             $table->string('email')->unique();
-            $table->foreignId('department_id')->nullable()->constrained()->references('id')->on('users');
             $table->rememberToken();
             $table->softDeletes();
-            $table->timestamps();
+            $table->timestamps(); 
         });
     }
 
